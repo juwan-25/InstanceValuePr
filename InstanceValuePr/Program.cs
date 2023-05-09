aad874c4d15a743961c05d9a9c96128036eda48d
Juwan Lee <ljw05w5@gmail.com>
2023-05-09 12:07:01 +0900

    리스트에 들어있는 데이터를 삭제할 때 주의할 점

--- InstanceValuePr/Program.cs
+++ InstanceValuePr/Program.cs
@@ -33,9 +33,47 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 new student() { name = "무민파파", grade = 3 }
             };
 
-            foreach (student student in list1)
+            foreach (var item in list1)
             {
-                Console.WriteLine(student);
+                Console.WriteLine(item);
+            }
+
+            // Remove기준이 객체임
+            // 기존 3개에서 할당하다가 할당하던 값이 사라짐 > 오류발생
+            /*foreach (var item in list1)
+            {
+                if (item.grade == 3)
+                {
+                    list1.Remove(item);
+                }
+            }
+
+            foreach (var item in list1)
+            {
+                Console.WriteLine(item);
+            }*/
+
+            // 순서가 이상해지는 문제 발생 > 뒤에서부터 삭제하면 됨 
+            /*for (int i = 0; i < 3; i++)
+            {
+                if (list1[i].grade == 3)
+                {
+                    list1.RemoveAt(1);
+                }
+            }*/
+
+            for (int i = list1.Count-1; i >= 0; i--)
+            {
+                if (list1[i].grade == 3)
+                {
+                    list1.RemoveAt(1);
+                }
+            }
+
+
+            foreach (var item in list1)
+            {
+                Console.WriteLine(item);
             }
         }
     }
